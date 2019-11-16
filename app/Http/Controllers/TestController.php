829f9e884704647ab42e6407a00a1d011074cffb
thuantran <thuantm2807@gmail.com>
2019-11-16 14:29:19 +0700

    sua doi thu vien graph

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,38 +14,40 @@ class TestController extends Controller
             'E' => array('B', 'D', 'F'),
             'F' => array('A', 'E', 'C'),
           );
-        $result = new Graph($graph);
+        $dest = new Graph($graph);
 
         // least number of hops between D and C
-        $result->breadthFirstSearch('D', 'C');
+        $result = $dest->breadthFirstSearch('D', 'C');
+        echo $dest->getLength($result). ' || '. $dest->getShortPath($result);
+        
         // outputs:
         // D to C in 3 hops
         // D->E->F->C
-        echo "<br/>";
-        // least number of hops between B and F
-        $result->breadthFirstSearch('B', 'F');
-        // outputs:
-        // B to F in 2 hops
-        // B->A->F
-        echo "<br/>";
+        // echo "<br/>";
+        // // least number of hops between B and F
+        // $result->breadthFirstSearch('B', 'F');
+        // // outputs:
+        // // B to F in 2 hops
+        // // B->A->F
+        // echo "<br/>";
 
-        // least number of hops between A and C
-        $result->breadthFirstSearch('A', 'C');
-        // outputs:
-        // A to C in 2 hops
-        // A->F->C
-        echo "<br/>";
+        // // least number of hops between A and C
+        // $result->breadthFirstSearch('A', 'C');
+        // // outputs:
+        // // A to C in 2 hops
+        // // A->F->C
+        // echo "<br/>";
 
-        // least number of hops between A and G
-        $result->breadthFirstSearch('A', 'G');
-        // outputs:
-        // No route from A to G
-        echo "<br/>";
+        // // least number of hops between A and G
+        // $result->breadthFirstSearch('A', 'G');
+        // // outputs:
+        // // No route from A to G
+        // echo "<br/>";
 
-        // least number of hops between A and G
-        $result->breadthFirstSearch('A', 'F');
-        // outputs:
-        // No route from A to G
+        // // least number of hops between A and G
+        // $result->breadthFirstSearch('A', 'F');
+        // // outputs:
+        // // No route from A to G
         echo "<br/>";
     }
 }
